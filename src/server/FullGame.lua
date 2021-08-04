ecc75d1c318386bf85969c2a447ce3249ed4ad98
starship006 <33463067+starship006@users.noreply.github.com>
2021-08-03 21:32:17 -0500

    Basic Room Loop
    Set up the framework for launching rooms and waiting for rooms to end.

--- src/server/FullGame.lua
+++ src/server/FullGame.lua
@@ -12,13 +12,16 @@ local AVERAGE_ROOMS_PER_ROUND_SERIES = 3
 
 
 
-
+--SERVIES AND MODULES
 local ServerScriptStorage = game:GetService("ServerScriptService")
 local Server = ServerScriptStorage:WaitForChild("Server")
 local PlayerInfo = require(Server:WaitForChild("PlayerInfo"))
+local Room = require(Server:WaitForChild("Room"))
 
 local ServerStorage = game:GetService("ServerStorage")
 local Bindables = ServerStorage:WaitForChild("Bindables")
+local RoomSetupFinished: BindableEvent = Bindables:WaitForChild("RoomSetupFinished")
+local RoomGameplayFinished: BindableEvent = Bindables:WaitForChild("RoomGameplayFinished")
 
 
 
@@ -32,7 +35,8 @@ function FullGame.new(players)
 
 
     newGame.Players = players
-    newGame.PlayerInfos = {} --dictionary key:player name, value: associated PlayerInfo object
+    newGame.PlayerInfos = {} --dictionary key:player name, player: associated PlayerInfo object
+    newGame.CurrentActiveRooms = {}  --array of active rooms
 
 
 
@@ -42,9 +46,13 @@ end
 function FullGame:Setup()
     --will generate all assets/information
     --setting up playerinfo objects
-    for key, value in ipairs(self.Players) do
-        local playerInfo = PlayerInfo.new(value)
-        self.PlayerInfos[value.Name] = playerInfo
+    game.Players.CharacterAutoLoads = false
+
+    for key, player in ipairs(self.Players) do
+        
+        --playerInfo stuff
+        local playerInfo = PlayerInfo.new(player)
+        self.PlayerInfos[player.Name] = playerInfo
     end
 end
 
@@ -56,14 +64,92 @@ end
 
 
 
-function FullGame:StartBossRoom()
 
 
+function FullGame:StartRoom(RoomType)
+    --right now we are just stuffing everyone in a room
+    local RoomToUse = Room:PickRandomRoom(#self.Players,RoomType)   
 
-end
 
-function FullGame:StartNormalRoom()
+    --creating all the rooms
+    local NewRoom: Room = Room.new(self.Players,RoomToUse)
+    table.insert(self.CurrentActiveRooms,NewRoom)
+
+    --throw rooms to tables for future reference
+    --this is in spawn to allow for the following code to wait
     
+    for index, RoomObject in ipairs(self.CurrentActiveRooms) do
+        RoomObject:SetupRoomAtLocation(index)       
+    end
+    
+    
+    
+    
+
+
+    --wait until all rooms setup (is this even necessary? i dont know man)
+    --[[while true do
+        RoomSetupFinished.Event:Wait()
+
+        --checking to see if all the rooms are setup
+        local ThereIsARoomNotFinishedSettingUp = false
+        for index, RoomObject in ipairs(self.CurrentActiveRooms) do
+            if not RoomObject:IsSetupFinished() then
+                ThereIsARoomNotFinishedSettingUp = true
+            end       
+    
+        end
+        if not ThereIsARoomNotFinishedSettingUp then
+            break
+        end
+
+        --if so, then the loop repeats, continually waiting on everything to be setup
+    end]]--
+    print("all rooms setup, starting gameplay")
+
+    --start play
+    spawn(function()
+        for index, RoomObject in ipairs(self.CurrentActiveRooms) do
+            RoomObject:InitiateStart() 
+    
+        end
+    end)
+
+
+    --wait until all rooms are finished with gameplay
+    while true do
+       RoomGameplayFinished.Event:Wait() 
+        --checking to see if all the rooms are finished
+        local ThereIsARoomNotFinishedPlaying = false
+        for index, RoomObject in ipairs(self.CurrentActiveRooms) do
+            if not RoomObject:IsGameplayFinished() then
+                ThereIsARoomNotFinishedPlaying = true
+            end       
+    
+        end
+        if not ThereIsARoomNotFinishedPlaying then
+             break
+        end
+    end
+    print("all rooms finished with gampelay, cleaning up")
+
+    --any post-round cleanup code for all the rooms can go here 
+    local winners = {}
+    local losers = {}
+    for index, RoomObject in ipairs(self.CurrentActiveRooms) do
+
+        for index2, winner in ipairs(RoomObject:GetWinners()) do
+            table.insert(winners,winner)
+        end
+        
+        for index2, loser in ipairs(RoomObject:GetLosers()) do
+            table.insert(winners,loser)
+        end
+        
+        RoomObject:Cleanup()
+    end
+    print(winners)
+    table.clear(self.CurrentActiveRooms)
 end
 
 
@@ -72,9 +158,12 @@ function FullGame:RoundSeries()
     for i = 1, AVERAGE_ROOMS_PER_ROUND_SERIES do  
         print("starting new round")      
         if i == AVERAGE_ROOMS_PER_ROUND_SERIES then
-            self:StartBossRoom()
+            --boss room
+            self:StartRoom(2)
+
         else
-            self:StartNormalRoom()
+            --normal room
+            self:StartRoom(1)
         end                  
         --by the end of this, the all rooms should've been completed and associated logic completed
     end
@@ -86,8 +175,8 @@ function FullGame:Start()
     --kicks off the first initial things for the game
 
 
-    
-    while #self.Players > 1 do
+    --start a round series if there are enough ppl
+    while #self.Players > 0 do
         self:RoundSeries()
     end
 
--- src/server/Room.lua
+++ src/server/Room.lua
@@ -2,39 +2,155 @@
 --each room object is a direct reference to a room object that is being actively used
 --by players. it will hold references to the players and everything
 
+
 local Room = {}
 Room.__index = Room
 
 
 
-Room.Rooms = {}   --[Room Type][Room Size][Index]   --Normal, Boss, Solo
+
+--script setup
+Room.Rooms = {{},{},{}}   --[Room Type][Room Size][Index]   --Normal, Boss, Solo
 local ServerStorage = game:GetService("ServerStorage")
 local Rooms = ServerStorage:WaitForChild("Rooms")
+local Bindables = ServerStorage:WaitForChild("Bindables")
+local RoomSetupFinished: BindableEvent = Bindables:WaitForChild("RoomSetupFinished")
+local RoomGameplayFinished: BindableEvent = Bindables:WaitForChild("RoomGameplayFinished")
+
+
 --setup of Room.Rooms table
 local rooms = Rooms:GetChildren()
 
 for key, room in ipairs(rooms) do
+
+    --allocating memory for each
+    if Room.Rooms[room:GetAttribute("RoomType")]==nil then
+        Room.Rooms[room:GetAttribute("RoomType")] = {}
+    end
+    if Room.Rooms[room:GetAttribute("RoomType")][room:GetAttribute("RoomSize")] == nil then
+        Room.Rooms[room:GetAttribute("RoomType")][room:GetAttribute("RoomSize")] = {}
+    end
+
+    --putting room in table
     table.insert(Room.Rooms[room:GetAttribute("RoomType")][room:GetAttribute("RoomSize")], room)
 end
 
+local function OnPlayerRemoving(player)
+    print(player.Name .. " is leaving")
+end
 
-function Room.new(players,roomType)
+function Room.new(newplayers:players,roomType:model)
     local newRoom = {}
     setmetatable(newRoom, Room)
 
-    newRoom.Players = players
-    newRoom.Room = nil   --this is the room object that will be copied over and over again (assigned in PickRoom)
-    newRoom:PickRoom(roomType)
+    newRoom.Players = newplayers
+    newRoom.Room = nil   --this is the room object that the players will actively fight in
+    newRoom.TemplateRoomReference = roomType --this is a stored reference room that we will copy
+    newRoom.TrackListeningEvents = {}
+    newRoom.Winners = {}
+    newRoom.Losers = {}
 
-
-
+    table.insert(newRoom.TrackListeningEvents, game.Players.PlayerRemoving:Connect(OnPlayerRemoving))
     return newRoom
 end
 
 
---picks a random room to play from
-function Room:PickRoom(roomType)
-    self.Room = Room.Rooms[roomType][#self.Players][math.random(#Room.Rooms[roomType][#self.Players])]
+--picks a random room to play from (should not be used by instances. this is static)
+function Room:PickRandomRoom(numPlayers: integer, roomType: integer)
+    --[[print(Room.Rooms[roomType])
+    print(Room.Rooms[roomType][numPlayers])]]--
+    print("roomType" .. roomType)
+    print("numPlayers" .. numPlayers)
+    local NewRoomType = Room.Rooms[roomType][numPlayers][math.random(#Room.Rooms[roomType][numPlayers])]
+    if NewRoomType == nil then
+        return error("nil newroomtype")
+    else        
+        return NewRoomType
+    end 
+end
+
+
+--generate room, move players, give players necessary items/choices for the room
+function Room:SetupRoomAtLocation(roomNumber:integer)
+    --generate room, move in workspace
+    self.Room = self.TemplateRoomReference:Clone()
+    self.Room.Parent = workspace
+
+    for index, player in ipairs(self.Players) do
+        
+        player:LoadCharacter()     --TODO: load the player at a spawnpoint
+        
+    end
+
+
+    wait(5)
+    self.Room:SetAttribute("SetupFinished", true)
+    RoomSetupFinished:Fire()
+end
+
+function Room:IsSetupFinished()
+    return self.Room:GetAttribute("SetupFinished")
+end
+
+function Room:IsGameplayFinished()
+    return self.Room:GetAttribute("GameplayFinished")
+end
+
+--send gui to players indicating start, give players their models, etc.
+function Room:InitiateStart()
+    --WinLogic is the module within each object that we run to collect the logic for everything
+    --local WinLogic = require(self.Room:WaitForChild("WinLogic"))
+    for index, player in ipairs(self.Players) do
+        player.Character.Humanoid.Died:Connect(function(character)       
+            print('player died')     
+            --HANDLE PLAYER DEATH CODE HERE
+            table.insert(self.Losers,player)
+            --WIN CONDITION 1: ALL OTHER PLAYERS DIE
+            if #self.Losers == #self.Players - 1 then
+                print("starting room finish")
+                self:InitiateRoomFinish()
+            else
+                print("room not finished yet")    
+            end
+        end)      
+        print(index)
+    end   
+
+    wait(3)
+
+end
+
+
+--called when the room gameplay is finished
+function Room:InitiateRoomFinish()
+    self.Room:SetAttribute("GameplayFinished", true)
+
+    --set winners (if you are not a loser, you are a winner)
+    for index, player in ipairs(self.Players) do
+        if player.Character then         --as you can see, we will need to make sure that the character dosent respawn until
+                                                    --hes supposed to start the next round
+            table.insert(self.Winners,player)
+        end
+    end
+
+    
+    RoomGameplayFinished:Fire()
+end
+
+--responsible for destroying room object, firing necessary events to clients that the round is over
+function Room:Cleanup()
+    self.Room:Destroy()
+    for index, eventObject in ipairs(self.TrackListeningEvents)  do
+        eventObject:Disconnect()
+    end
+end
+
+function Room:GetWinners()
+    return self.Winners
+end
+
+function Room:GetLosers()
+    return self.Losers
 end
 
 
